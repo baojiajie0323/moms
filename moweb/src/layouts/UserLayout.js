c8d01c12dd44307dc23ea4e67e5f68f17408af6b
baojiajie0323 <382058502@qq.com>
2018-08-13 17:27:15 +0800

    aaaa

--- moweb/src/layouts/UserLayout.js
+++ moweb/src/layouts/UserLayout.js
@@ -77,7 +77,7 @@ class UserLayout extends React.PureComponent {
               <Redirect from="/user" to={getLoginPathWithRedirectPath()} />
             </Switch>
           </div>
-          <GlobalFooter links={links} copyright={copyright} />
+          <GlobalFooter links={null} copyright={copyright} />
         </div>
       </DocumentTitle>
     );
